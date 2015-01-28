86796160214da7421e37c1b38a5e628837cb67e5
Jacob Mathison <jakethewizard04@gmail.com>
2015-01-27 22:04:49 -0600

    We must go deeper...
    Yay for Laravel's blade to make this painfully clean. Not yay for
    figuring out infrastructure.... But I think that is my fault tbh.
    
    In other news.... I may have everything modeled for relationships to
    work quite nicely and smoothly. Begs the question of how much deeper one
    can go.... before putting at risk of making a God Object, (God forbid,
    HA!)
    
    http://en.wikipedia.org/wiki/God_object

--- resources/views/superAdmin/viewObjectItem.blade.php
+++ resources/views/superAdmin/viewObjectItem.blade.php
@@ -3,12 +3,16 @@
 @section('content')
 	<h1>{{ $objectName }}</h1>
 	
-	
 	@foreach ($sharedData['primary'] as $data)
-		<h3>{{ $data['object'] }}</h3>
 		@foreach ($data as $key => $units)
-			@if ($key != 'object')
-				$units->id;
+			@if ($key === 'object')
+				<h3>{{ $units }}</h3>
+			@elseif ($key !== 'object')
+				@foreach ($units as $newKey => $unit)
+					@foreach ($unit as $details => $dividedUnit)
+						{{ $details }}
+					@endforeach
+				@endforeach
 			@endif
 		@endforeach
 	@endforeach
