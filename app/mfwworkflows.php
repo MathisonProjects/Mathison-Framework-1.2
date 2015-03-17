5ad21546d186ba1ce628df5f5e3fd784c849a887
Jacob Mathison <jmathison@bitbasic.com>
2015-03-17 15:48:53 -0500

    Fresh Commit

--- app/mfwworkflows.php
+++
@@ -1,8 +0,0 @@
-<?php namespace App;
-	
-	use Illuminate\Database\Eloquent\Model as Eloquent;
-
-	class mfwworkflows extends Eloquent {
-		
-	}
-?>
--- public/js/submenu-activate.js
+++
@@ -1,4 +0,0 @@
-$( document ).ready(function() {
-  console.log("Submenu Ready!");
-  $('.dropdown-submenu > a').submenupicker();
-});
