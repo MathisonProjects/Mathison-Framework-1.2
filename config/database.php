ecd84bd1588c904ae6997232b0fa4c320e30351d
Jacob Mathison <jmathison@bitbasic.com>
2015-03-17 16:28:09 -0500

    Revert "test"
    This reverts commit 11b506794de016e6c12dec9bf765c4a08113df8d.

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 	| All database work in Laravel is done through the PHP PDO facilities
 	| so make sure you have the driver for your particular database of
 	| choice installed on your machine before you begin development.
-	| 
+	|
 	*/
 
 	'connections' => [
