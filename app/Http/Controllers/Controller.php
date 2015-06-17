75f34560b70aa9e0cf53ef0310eddef4b5aa9034
Jacob Mathison <jmathison@bitbasic.com>
2015-06-17 16:52:16 -0500

    Controller.php refactored. Eliminates repetitive reuse of 'use App\MODULENAME' ideally. Testing idea next.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -17,21 +17,41 @@ use DB;
 abstract class Controller extends BaseController {
 
 	use DispatchesCommands, ValidatesRequests;
-
-	public $menu;
-	public $post;
+    public  $module;
+	public  $menu;
+	public  $post;
 
     public function __construct() {
-        $this->menu['objects']        = mfwobjects::where('oid', 0)->get();
-        $this->menu['workflows']      = mfwworkflows::get();
-        $this->menu['relationships']  = mfwobjectrelationships::get();
-        $this->menu['forms']          = mfwmanageforms::where('fid', 0)->get();
-        $this->menu['apis']           = mfwapis::get();
-        $this->menu['formprocessing'] = mfwformprocessings::get();
-        $this->menu['templates']      = mfwtemplates::get();
-        $this->menu['pages']          = mfwpages::get();
+        $this->loadModule();
+        $this->loadMenu();
+
         if (isset($_POST)) {
             $this->post = $_POST;
         }
     }
+
+    private function loadModule() {
+        $this->module['objects']        = new mfwobjects;
+        $this->module['forms']          = new mfwmanageforms;
+        $this->module['workflows']      = new mfwworkflows;
+        $this->module['relationships']  = new mfwobjectrelationships;
+        $this->module['apis']           = new mfwapis;
+        $this->module['formprocessing'] = new mfwformprocessings;
+        $this->module['templates']      = new mfwtemplates;
+        $this->module['pages']          = new mfwpages;
+    }
+
+    private function loadMenu() {
+        foreach ($this->module as $key => $module) {
+            if ($key == 'objects') {
+                $this->menu[$key] = $module->where('oid', 0)->get();
+            } else if ($key == 'forms') {
+                $this->menu[$key] = $module->where('fid', 0)->get();
+            } else {
+                $this->menu[$key] = $module->get();
+                
+            }
+        }
+    }
+
 }
