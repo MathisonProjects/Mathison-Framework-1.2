47c43a33f59f5383b2c5dcdaeecc184bd30e929b
Jacob Mathison <jmathison@bitbasic.com>
2015-06-29 14:34:58 -0500

    API menu...

--- resources/views/superAdmin/menu/list/apis.blade.php
+++ resources/views/superAdmin/menu/list/apis.blade.php
@@ -2,16 +2,16 @@
 	<a tabindex="0" data-toggle="dropdown">APIs</a>
 	<ul class="dropdown-menu">
 		<li>
-			<a tabindex="0" href="/admin/super/api/create">Create API</a>
+			<a tabindex="0" href="/admin/super/apis/create">Create API</a>
 		</li>
 		<li>
-			<a tabindex="0" href="/admin/super/api">View APIs</a>
+			<a tabindex="0" href="/admin/super/apis">View APIs</a>
 		</li>
 		<li></li>
 		<li class="divider"></li>
 		@foreach ($menu['apis'] as $item)
 		<li>
-			<a tabindex="0" href="/admin/super/api/{{ $item->id }}">
+			<a tabindex="0" href="/admin/super/apis/{{ $item->id }}">
 				<?php
 				echo ucwords(str_replace('_', ' ',$item->name));
 				?></a>
