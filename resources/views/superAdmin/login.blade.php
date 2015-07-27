a4424828c6463b1ec3c02a3dcd56546936268064
Jacob Mathison <jmathison@bitbasic.com>
2015-07-27 14:57:52 -0500

    Added AngularJS. Superadmin Login locked if empty. Defaults to admin as username.

--- resources/views/superAdmin/login.blade.php
+++ resources/views/superAdmin/login.blade.php
@@ -5,9 +5,9 @@
 		<h2>{{ session('Logout') }}</h2>
 	@endif
 	@if ($count == 0)
-		{!! Form::open(['url'=>'admin/super/createAdmin/']) !!}
+		{!! Form::open(['url'=>'admin/super/createAdmin/', 'ng-app' => 'LoginForm', 'ng-controller' => 'LoginController', 'name' => 'Login', 'novalidate' => '']) !!}
 	@else
-		{!! Form::open(['url'=>'admin/super/adminLogin/']) !!}
+		{!! Form::open(['url'=>'admin/super/adminLogin/', 'ng-app' => 'LoginForm', 'ng-controller' => 'LoginController', 'name' => 'Login', 'novalidate' => '']) !!}
 	@endif
 	<div class='row'>
 		<div class='col-md-4'></div>
@@ -17,20 +17,34 @@
 				<h6>{{ $message }}</h6>
 			</div>
 			<div class='col-md-12'>
-				{!! Form::label('email', 'Email') !!}
-				{!! Form::text('email', null, array('id' => 'email', 'placeholder' => 'Email', 'class' => 'form-control', 'maxlength' => '25')) !!}
+				{!! Form::label('email', 'Email or Username') !!}
+				{!! Form::text('email', null, array('id' => 'email', 'placeholder' => 'Email', 'class' => 'form-control', 'maxlength' => '25', 'ng-model' => 'email', 'required' => '')) !!}
+				  <span style="color:red" ng-show="Login.email.$invalid">
+				  	<span ng-show="Login.email.$error.required">Email or Username is required.</span>
+				  </span>
 			</div>
 			<div class='col-md-12'>
 				<br />
 				{!! Form::label('password', 'Password') !!}
-				{!! Form::password('password', array('id' => 'password', 'placeholder' => 'Password', 'class' => 'form-control', 'maxlength' => '25')) !!}
+				{!! Form::password('password', array('id' => 'password', 'placeholder' => 'Password', 'class' => 'form-control', 'maxlength' => '25', 'ng-model' => 'password', 'required' => '')) !!}
+				  <span style="color:red" ng-show="Login.password.$invalid">
+				  	<span ng-show="Login.password.$error.required">Password is required.</span>
+				  </span>
 			</div>
 			<div class='col-md-12'>
 				<br />
-				{!! Form::submit('Submit', ['class' => 'btn btn-primary col-md-12']) !!}
+				{!! Form::submit('Submit', ['class' => 'btn btn-primary col-md-12', 'ng-disabled' => 'Login.email.$invalid || Login.password.$invalid']) !!}
 			</div>
 			
 		</div>
 	</div>
 	{!! Form::close() !!}
+
+	<script>
+		var app = angular.module('LoginForm', []);
+		app.controller('LoginController', function($scope) {
+		    $scope.email = 'admin';
+		    $scope.password = '';
+		});
+	</script>
 @stop
--- resources/views/superAdmin/master/js.blade.php
+++ resources/views/superAdmin/master/js.blade.php
@@ -4,3 +4,4 @@
 <script src='/js/submenu-activate.js'></script>
 <script src='/js/jquery.dataTables.min.js'></script>
 <script src='//cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js'></script>
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
