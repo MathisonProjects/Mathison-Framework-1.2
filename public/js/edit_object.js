b44c24ee22dc16972351bf628162e2cdc75bf5a1
Jacob Mathison <jakethewizard04@gmail.com>
2015-08-13 21:08:29 -0500

    @MFW1.2 Started edit columns removing/adding fields

---
+++ public/js/edit_object.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  console.log("edit_object.js Ready!");
+
+});
--- resources/views/superAdmin/modules/objects/editColumns.blade.php
+++ resources/views/superAdmin/modules/objects/editColumns.blade.php
@@ -4,25 +4,35 @@
 | Edit Object Columns
 @endsection
 
+@section('header')
+	<script src='/js/edit_object.js'></script>
+@stop
+
 @section('content')
 	<h2>Edit Object Columns</h2>
 	{!! Form::open(['ng-app' => 'ColumnsApp', 'ng-controller' => 'ColumnsController', 'name' => 'Columns', 'novalidate' => '']) !!}
 	<div class='row'>
 		<div class='col-md-4'>
-	@foreach ($object as $field)
-		@if ($field->name != 'id')
-		<div class='form-group'>
-			{!! Form::label($field->name, 'Old Name: '.$field->name) !!}
-			{!! Form::text($field->name, '', array('id' => $field->name, 'placeholder' => $field->name, 'class' => 'form-control', 'ng-model' => $field->name, 'required' => '')) !!}
-			<span style="color:red" ng-show="Columns.{{ $field->name }}.$invalid">
-				  <span ng-show="Columns.{{ $field->name }}.$error.required">Object Name is required.
-			</span>
+			@foreach ($object as $field)
+				@if ($field->name != 'id')
+				<div class='form-group'>
+					{!! Form::label($field->name, 'Old Name: '.$field->name) !!}
+					{!! Form::text($field->name, '', array('id' => $field->name, 'placeholder' => $field->name, 'class' => 'form-control', 'ng-model' => $field->name, 'required' => '')) !!}
+					{!! Form::checkbox($field->name.'[Delete]', 'Delete', false) !!} Delete?
+					<br />
+					<span style="color:red" ng-show="Columns.{{ $field->name }}.$invalid">
+						  <span ng-show="Columns.{{ $field->name }}.$error.required">Field Name is required.
+					</span>
+				</div>
+				@endif
+			@endforeach
+			<div class='form-group'>
+				{!! Form::submit('Submit', ['class' => 'btn btn-primary col-md-12 addField']) !!}
+			</div>
 		</div>
-		@endif
-	@endforeach
-		<div class='form-group'>
-			{!! Form::submit('Submit', ['class' => 'btn btn-primary col-md-12']) !!}
-		</div>
+		<div class='col-md-4'>
+			{!! Form::button('Add Field', ['class' => 'btn btn-primary col-md-12']) !!}
+			{!! Form::button('Reset Fields', ['class' => 'btn btn-primary col-md-12']) !!}
 		</div>
 	</div>
 	{!! Form::close() !!}
