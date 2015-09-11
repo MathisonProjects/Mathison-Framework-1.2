1c3c348fb50059ab2eaf0b4dda9ac36fbd7417e1
Jacob Mathison <jakethewizard04@gmail.com>
2015-09-10 22:10:13 -0500

    @MFW1.2 Template improvements. Easier to read/improve.

--- resources/views/templates/bare.blade.php
+++ resources/views/templates/bare.blade.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="Jacob Mathison">
-
-        <title>{{ $constant['title'] }}</title>
+        @include('templates.metaheader')
 
         <link href="css/bootstrap.min.css" rel="stylesheet">
 
@@ -18,12 +11,6 @@
             /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
         }
         </style>
-        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
 
     </head>
     <body>
@@ -51,9 +38,8 @@
         </nav>
         <div class="container">
             <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h1>{{$header}}</h1>
-                    @include($file)
+                <div class="col-lg-12">
+                    @yield('content')
                 </div>
             </div>
             <hr>
