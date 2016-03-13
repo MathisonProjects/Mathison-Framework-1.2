10f381728cc4806f6d9719635f2d4b197d2cb1aa
Jacob Mathison <jacob@mathisonprojects.com>
2016-03-13 14:54:26 -0500

    Filled out landing page generator more

--- resources/views/superAdmin/modules/landingPages/form.blade.php
+++ resources/views/superAdmin/modules/landingPages/form.blade.php
@@ -21,26 +21,69 @@
 {!! Form::close() !!}
 
 <div class='row'>
-	<div class='col-md-9'>Landing Page Test View</div>
+	<div class='col-md-9' id='landingPage' style='height: 100%;'>
+		<div class='row'>
+			<div class='col-md-4' id='logoPlaceholder'></div>
+			<div class='col-md-8' id='headlinePlaceholder'>Landing Page Test View</div>
+		</div>
+	</div>
 	<div class='col-md-3' id='dataCollection'>
 		<div class='form-group'>
 			{!! Form::label('background', 'Background') !!}
-			{!! Form::text('background', null, ['class' => 'form-control', 'id' => 'background']) !!}
+			{!! Form::text('background', null, ['class' => 'form-control', 'id' => 'background', 'placeholder' => 'URL for your background']) !!}
 		</div>
 		<div class='form-group'>
 			{!! Form::label('logo', 'Logo') !!}
-			{!! Form::text('logo', null, ['class' => 'form-control', 'id' => 'logo']) !!}
+			{!! Form::text('logo', null, ['class' => 'form-control', 'id' => 'logo', 'placeholder' => 'URL for your logo']) !!}
 		</div>
 		<div class='form-group'>
 			{!! Form::label('headline', 'Headline') !!}
-			{!! Form::text('headline', null, ['class' => 'form-control', 'id' => 'headline']) !!}
+			{!! Form::text('headline', null, ['class' => 'form-control', 'id' => 'headline', 'maxlength' => '28', 'placeholder' => 'What is your headline?']) !!}
 		</div>
 		<div class='form-group'>
 			{!! Form::label('section', 'Section') !!}
 			{!! Form::select('section', array('' => '', 'form' => 'Form', 'text' => 'Text', 'video' => 'Video'), null, ['class' => 'form-control', 'id' => 'section']) !!}
 		</div>
+		<div class='form-group' id='form'>
+			{!! Form::label('form', 'Form') !!}
+			{!! Form::text('form', null, ['class' => 'form-control']) !!}
+		</div>
+		<div class='form-group' id='text'>
+			{!! Form::label('text', 'Text') !!}
+			{!! Form::text('text', null, ['class' => 'form-control']) !!}
+		</div>
+		<div class='form-group' id='video'>
+			{!! Form::label('video', 'Youtube URL') !!}
+			{!! Form::text('video', null, ['class' => 'form-control']) !!}
+		</div>
+		<div id='additionalSections'></div>
 		<div class='form-group'>
-			<a href='javascript:void(0)' class='btn btn-primary col-md-12'>Add Section</a>
+			<a href='#' id='addSection' class='btn btn-primary col-md-12'>Add Section</a>
 		</div>
 	</div>
 </div>
+
+<script type="text/javascript">
+	$('#form').hide();
+	$('#text').hide();
+	$('#video').hide();
+
+	$('#background').keyup(function() {
+		$('#landingPage').css('background-image', 'url('+$('#background').val()+')');
+	});
+
+	$('#logo').keyup(function() {
+		$('#logoPlaceholder').html('<img src="'+$('#logo').val()+'" class="col-md-12" />');
+	});
+
+	$('#headline').keyup(function() {
+		$('#headlinePlaceholder').html('<h1>'+$('#headline').val()+'</h1>');
+	});
+
+	$('#section').change(function() {
+		$('#form').hide();
+		$('#text').hide();
+		$('#video').hide();
+		$('#'+$("#section").val()).show();
+	});
+</script>
